beb6754b841cc74c68354a02851dec9c3e4e7ebc
Vignesh Lakshmanan <49462691+weekjeff@users.noreply.github.com>
2025-02-10 16:59:32 -0500

    added forward pass during gradient step

--- mark2.py
+++ mark2.py
@@ -1,27 +1,16 @@
-from typing import Optional, List
-
 class Optimizer:
     def __init__(self, learning_rate=0.01):
-        self.learning_rate = learning_rate
+
+    # tells us what direction to go
+    def compute_loss(self, node):
 
     def take_gradient_step(self, node):
-        if node.gradient:
-            node.data -= self.learning_rate * node.gradient
-
-        for input_node in node.input_nodes:
-            self.take_gradient_step(input_node)
-
-        if node.op:
-            node.op.forward()
-
-
-
 
 
 # Computation Graph Node
 class Node:
 
-    def __init__(self, data, op: Optional["Operation"] = None, input_nodes=None, gradient=0):
+    def __init__(self, data, op=None, input_nodes=None, gradient=0):
         self.data = data
         self.gradient = gradient
 
@@ -31,6 +20,17 @@ class Node:
 
     def __str__(self):
         return f'Data: {self.data}'
+
+    # instantiates back prop
+    def compute_gradient(self):
+        # recursively zeroes the gradients
+        self.zero_gradient()
+
+        # calculate gradient
+        self.autograd()
+
+        # take gradient step
+        self.optimizer.take_gradient_step(self)
 
     # reset the gradient
     def zero_gradient(self):
@@ -60,24 +60,13 @@ class LossNode(Node):
     def __init__(self, data, op=None, input_nodes=None, gradient=0):
         super().__init__(data, op, input_nodes, gradient)
 
-    # instantiates back prop
-    def compute_gradient(self):
-        # recursively zeroes the gradients
-        self.zero_gradient()
-
-        # calculate gradient
-        self.autograd()
-
-        # take gradient step
-        self.optimizer.take_gradient_step(self)
-
 
 # interface for forward and backward
 class Operation:
     def __init__(self):
         pass
 
-    def forward_node(self):
+    def forward(self):
         raise NotImplementedError("Implement Forward Function")
 
     def backward(self, grad):
@@ -87,15 +76,10 @@ class Operation:
 class Add(Operation):
     def __init__(self, *nodes):
         self.nodeList = list(nodes)
-        self.node = None
-
-    def forward_node(self):
-        self.node = Node(sum(node.data for node in self.nodeList), op=self,
-                    input_nodes=self.nodeList)
-        return self.node
 
     def forward(self):
-        self.node.data = sum(node.data for node in self.nodeList)
+        return Node(sum(node.data for node in self.nodeList), op=self,
+                    input_nodes=self.nodeList)
 
     def backward(self, grad):
         return [grad for _ in self.nodeList]
@@ -105,35 +89,26 @@ class MSELoss(Operation):
     def __init__(self, prediction_node, ground_truth):
         self.prediction_node = prediction_node
         self.ground_truth = ground_truth
-        self.node = None
-
-    def forward_node(self):
-        loss = (self.prediction_node.data - self.ground_truth) ** 2
-        self.node = LossNode(loss, self, [self.prediction_node])
-        return self.node
 
     def forward(self):
         loss = (self.prediction_node.data - self.ground_truth) ** 2
-        self.node.data = loss
+        return LossNode(loss, self, [self.prediction_node])
 
     def backward(self, grad=1):
-            grad = 2 * (self.prediction_node.data - self.ground_truth)
-            return [grad]
-
+        grad = 2 * (self.prediction_node.data - self.ground_truth)
+        return [grad]
 
 # create prediction node
 n1 = Node(5)
-n2 = Node(2)
-n3 = Add(n1, n2).forward_node()
 # create loss node
-mse_loss_node = MSELoss(n3, 6).forward_node()
+mse_loss_node = MSELoss(n1, 6).forward()
 
 # check initial loss
 print(mse_loss_node)
-for i in range(50):
+# do backprop
+for i in range(500):
     mse_loss_node.compute_gradient()
-    print(f"\non iteration {i+1}")
+    print(f"\non iteration {i}")
     print(mse_loss_node)
     print(n1)
-    print(n2)
-    print(n3)
+
